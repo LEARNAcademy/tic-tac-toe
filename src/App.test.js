67b242a59eebcb6ee71df332fe819a9ef41bbd49
Sarah Proctor <sproctor950@gmail.com>
2022-08-20 14:42:47 -0700

    :white_check_mark: adds base test in react testing library

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders tic tac toe header', () => {
+  render(<App/>);
+  const header = screen.getByText('Tic Tac Toe'); 
 });
